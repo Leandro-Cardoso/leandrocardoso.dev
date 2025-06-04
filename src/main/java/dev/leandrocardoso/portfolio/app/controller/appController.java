81baa894b6612e92fecc77b8f6231a3e6e762bb5
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-06-04 01:52:54 -0300

    Header links actives

--- src/main/java/dev/leandrocardoso/portfolio/app/controller/appController.java
+++ src/main/java/dev/leandrocardoso/portfolio/app/controller/appController.java
@@ -1,33 +1,39 @@
 package dev.leandrocardoso.portfolio.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class appController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("activePage", "home");
         return "index";
     }
 
     @GetMapping("/about")
-    public String about() {
+    public String about(Model model) {
+        model.addAttribute("activePage", "about");
         return "about";
     }
 
     @GetMapping("/contacts")
-    public String contacts() {
+    public String contacts(Model model) {
+        model.addAttribute("activePage", "contacts");
         return "contacts";
     }
 
     @GetMapping("/projects")
-    public String projects() {
+    public String projects(Model model) {
+        model.addAttribute("activePage", "projects");
         return "projects";
     }
 
     @GetMapping("/blog")
-    public String blog() {
+    public String blog(Model model) {
+        model.addAttribute("activePage", "blog");
         return "blog";
     }
 
