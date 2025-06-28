434c290a9960d75999b810d26e79b74f90e47b35
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-06-28 20:53:11 -0300

    Implement Language

--- src/main/java/dev/leandrocardoso/portfolio/controller/AboutController.java
+++ src/main/java/dev/leandrocardoso/portfolio/controller/AboutController.java
@@ -2,8 +2,10 @@ package dev.leandrocardoso.portfolio.controller;
 
 import dev.leandrocardoso.portfolio.model.AcademicQualification;
 import dev.leandrocardoso.portfolio.model.Certification;
+import dev.leandrocardoso.portfolio.model.Language;
 import dev.leandrocardoso.portfolio.service.AcademicQualificationService;
 import dev.leandrocardoso.portfolio.service.CertificationService;
+import dev.leandrocardoso.portfolio.service.LanguageService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,13 +22,16 @@ public class AboutController {
 
     private final AcademicQualificationService academicQualificationService;
     private final CertificationService certificationService;
+    private final LanguageService languageService;
 
     @Autowired
     public AboutController(AcademicQualificationService academicQualificationService,
-                           CertificationService certificationService) {
+                           CertificationService certificationService,
+                           LanguageService languageService) {
 
         this.academicQualificationService = academicQualificationService;
         this.certificationService = certificationService;
+        this.languageService = languageService;
 
     }
 
@@ -35,12 +40,15 @@ public class AboutController {
 
         List<AcademicQualification> academicQualifications = academicQualificationService.getAllAcademicQualifications();
         List<Certification> certifications = certificationService.getAllCertifications();
+        List<Language> languages = languageService.getAllLanguages();
 
         Collections.reverse(academicQualifications);
         Collections.reverse(certifications);
+        Collections.reverse(languages);
 
         model.addAttribute("academicQualifications", academicQualifications);
         model.addAttribute("certifications", certifications);
+        model.addAttribute("languages", languages);
 
         model.addAttribute("activePage", "about");
         model.addAttribute("request", request);
